09ce19ef79fc83144d35c827715dc01af0f484a7
mrlbni <suchitamiki06@gmail.com>
2026-03-15 12:24:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,18 @@
 
 set -euo pipefail
 
-printf "\n🔥 unzip-bot 🔥\n\n"
+printf "\n🔥 unzip-bot 🔥\n\n» Copyright (c) 2022 - present EDM115\n» MIT License\n\n»» Join @EDM115bots on Telegram\n»» Follow EDM115 on GitHub\n\n"
 
 if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
+  if grep -qE '^[^#]*=\s*("|'\''?)\s*\1\s*$' .env; then
+    printf "❗ Some required vars are empty, please fill them unless you're filling them somewhere else\n"
+  else
+    while IFS='=' read -r key value; do
+      if [[ ! $key =~ ^# && -n $key ]]; then
+        export "$key=$value"
+      fi
+    done < .env
+  fi
 fi
 
 exec python -m unzipbot
