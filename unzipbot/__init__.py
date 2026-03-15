a68d51959fbd3e96af04cd8f7442a84f35c3208b
mrlbni <suchitamiki06@gmail.com>
2026-03-15 12:55:24 +0530

    Update __init__.py

--- unzipbot/__init__.py
+++ unzipbot/__init__.py
@@ -3,11 +3,31 @@ import logging
 import sys
 import time
 
-from pyrogram import Client
+# -------- Logging setup first (avoid circular import issues) --------
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[
+        logging.FileHandler("unzip-bot.log"),
+        logging.StreamHandler()
+    ],
+    format="%(asctime)s - %(levelname)s - %(name)s - %(threadName)s - %(message)s",
+)
 
+LOGGER = logging.getLogger("unzipbot")
+
+logging.getLogger("aiohttp").setLevel(logging.WARNING)
+logging.getLogger("aiofiles").setLevel(logging.WARNING)
+logging.getLogger("GitPython").setLevel(logging.WARNING)
+logging.getLogger("motor").setLevel(logging.WARNING)
+logging.getLogger("Pillow").setLevel(logging.WARNING)
+logging.getLogger("psutil").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+# -------- Other imports after logger --------
+from pyrogram import Client
 from .config.config import Config
 
-# https://stackoverflow.com/questions/69890200/how-to-configure-os-specific-dependencies-in-a-pyproject-toml-file-maturin/75711133#75711133
+# -------- Event loop setup --------
 if sys.platform.startswith("win32") or sys.platform.startswith("linux-cross"):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -21,6 +41,7 @@ else:
 boottime = time.time()
 plugins = dict(root="modules")
 
+# -------- Pyrogram client --------
 unzipbot_client = Client(
     name="unzip-bot",
     bot_token=Config.BOT_TOKEN,
@@ -30,19 +51,3 @@ unzipbot_client = Client(
     sleep_threshold=7200,
     max_concurrent_transmissions=3,
 )
-
-logging.basicConfig(
-    level=logging.INFO,
-    handlers=[logging.FileHandler(filename="unzip-bot.log"), logging.StreamHandler()],
-    format="%(asctime)s - %(levelname)s - %(name)s - %(threadName)s - %(message)s",
-)
-
-LOGGER = logging.getLogger(__name__)
-
-logging.getLogger("aiohttp").setLevel(logging.WARNING)
-logging.getLogger("aiofiles").setLevel(logging.WARNING)
-logging.getLogger("GitPython").setLevel(logging.WARNING)
-logging.getLogger("motor").setLevel(logging.WARNING)
-logging.getLogger("Pillow").setLevel(logging.WARNING)
-logging.getLogger("psutil").setLevel(logging.WARNING)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
